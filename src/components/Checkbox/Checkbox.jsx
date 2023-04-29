809e8b0bbc6dc3017c08d31bada24c822ecbfd9d
deleonroselle21 <deleonroselle21@gmail.com>
2023-04-29 14:24:38 -0400

    add mobile breakpoints

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -5,7 +5,7 @@ import './checkbox.css'
 const Checkbox = (props) => {
   return (
     <div>
-         <label className="checkbox__container">{props.text}
+         <label className="checkbox__container"><h1>{props.text}</h1>
   <input type="checkbox" name={props.name} checked={props.checked} onChange={props.onChange/*()=>setcheckBoxValue(!checkBoxValue.includesLowercase)*/}/>
   <span className="checkmark"></span>
   {/*console.log(props.checked)*/}
